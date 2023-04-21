60524108f0054938a8cd6ab87434de45b84ce2f9
Raul Zannoni <130564115+raulzannoni@users.noreply.github.com>
2023-04-21 21:39:22 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,28 @@
+<h1>POO_Cinema</h1>
+
+<h3>****************************</h3>
+
+<?php
+
+spl_autoload_register(function ($class_name) 
+    {
+        include 'classes/' . $class_name . '.php';
+    });
+
+    $SF = new FilmType("SF");
+    $Drama = new FilmType("Drama");
+    
+    $RidleyScott = new Director("Scott", "Ridley", "masculin", "30-11-1937");
+
+
+
+    $Alien = new Film("Alien", "01-01-1979", 117, $RidleyScott, $SF);
+    $Blade_Runner = new Film("Blade Runner", "01-01-1982", 111, $RidleyScott, $SF);
+    $Gladiator = new Film("Gladiator", "01-01-2000", 155, $RidleyScott, $Drama);
+
+
+    $RidleyScott->getFilmographie();
+
+    $SF->getFilms();  
+
+?>
