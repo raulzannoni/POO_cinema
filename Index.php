6343f14ca5eb7d259f7d3b6dd48e016aadd434d5
raul_zannoni <raul.zannnoni92@gmail.com>
2023-04-22 09:17:28 +0200

    Creation des nouveaux objets

--- Index.php
+++ Index.php
@@ -11,18 +11,39 @@ spl_autoload_register(function ($class_name)
     });
 
     $SF = new FilmType("SF");
-    $Drama = new FilmType("Drama");
-    
-    $RidleyScott = new Director("Scott", "Ridley", "masculin", "30-11-1937");
+    $Drama = new FilmType("Dramatique");
+    $Aventure = new FilmType("Aventure");
 
+
+    
+    $RidleyScott = new Director("Scott", "Ridley", "homme", "30-11-1937");
+    $StevenSpielberg = new Director("Spielberg", "Lucas", "homme", "18-12-1946");
+    $RobertZemeckis = new Director("Zemeckis", "Robert", "homme", "14-05-1951");
 
 
     $Alien = new Film("Alien", "01-01-1979", 117, $RidleyScott, $SF);
     $Blade_Runner = new Film("Blade Runner", "01-01-1982", 111, $RidleyScott, $SF);
     $Gladiator = new Film("Gladiator", "01-01-2000", 155, $RidleyScott, $Drama);
+    $Indiana_Jones = new Film("Indiana Jones - Les Aventuriers de l'arche perdue", "01-01-1981", 115, $StevenSpielberg, $Aventure);
+    $Soldat_Ryan = new Film("Il faut sauver le soldat Ryan", "01-01-1998", 163, $StevenSpielberg, $Drama);
+    $Minority_Report = new Film("Minority Report", "01-01-2002", 145, $StevenSpielberg, $SF);
+    $Retour_Futur = new Film("Retour vers le futur", "01-01-1985", 116, $RobertZemeckis, $SF);
+    $Forrest_Gump = new Film("Forrest Gump", "01-01-1994", 142, $RobertZemeckis, $Drama);
+    $Cast_Away = new Film("Seul au monde", "01-01-2000", 143, $RobertZemeckis, $Drama);
+
+
+
+
+
+
 
     $RidleyScott->getFilmographie();
+    $StevenSpielberg->getFilmographie();
+    $RobertZemeckis->getFilmographie();
 
-    $SF->getFilms();  
+
+
+    $SF->getFilms(); 
+    $Drama->getFilms(); 
 
 ?>
